9ec2c739d24b37ee7a52a29386ce71247c30cb48
Jesus Rosas <infinitdeveloperofficial@gmail.com>
2016-05-25 16:33:42 -0500

    vistas parciales

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,9 @@ class ArticlesController < ApplicationController
 	def new 
 		@article = Article.new
 	end
+	def edit 
+		@article = Article.find(params[:id])
+	end
 	#POST /articles
 	def create
 		@article = Article.new(article_params)
@@ -27,6 +30,12 @@ class ArticlesController < ApplicationController
 	end
 	#PUT /articles/:id
 	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			render_to @article
+		else
+			render :edit
+		end
 	end
 	#Private es una palabra reservada que determina que métodos son de tipo privado por lo que 
 	#todo lo que se encuentra debajo de esa palabra se considera como un método o propiedad privada
