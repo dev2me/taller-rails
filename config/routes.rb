58d7f08c7f7403bf4a2ce0fc8c26923a961fc4a7
Jesus Rosas <infinitdeveloperofficial@gmail.com>
2016-05-30 19:34:02 -0500

    manage data for ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :articles do
-    resources :comments
+    resources :comments, only: [:create, :destroy, :update]
   end
   devise_for :users
   root 'welcome#index'
